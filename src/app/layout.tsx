2bfffb0194497b155c59be2ba16f64b42eed06d2
Rafael Aguilar <rafaguilar@gmail.com>
2025-10-18 05:57:34 +0000

    I want run SimpleAnalytics
    Install via HTML
    Include this line at the en

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,8 @@ export default function RootLayout({
         </Suspense>
         {children}
         <Toaster />
+        {/* 100% privacy-first analytics */}
+        <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
       </body>
     </html>
   );
